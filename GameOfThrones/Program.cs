dea596cce8585d7507d5eabbb7096eda341c9b18
Riham Abbady Ali <r_abbady@icloud.com>
2021-07-23 14:51:13 +0200

    game of thrones problesm

---
+++ GameOfThrones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfThrones
+{
+    class Program
+    {
+        public static string gameOfThrones(string s)
+        {
+            int count = 0;
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++) {
+                if (freq.ContainsKey(s[i]))
+                    freq[s[i]] = freq[s[i]] + 1;
+                else
+                    freq.Add(s[i], 1);
+
+            }
+            //check if elements are even or odd
+            foreach(var f in freq)
+            {
+                count += f.Value%2;
+            }
+            if (count> 1)
+                return("NO");
+            else
+                return("YES");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(gameOfThrones("cdefghmnopqrstuvw"));
+        }
+    }
+}
