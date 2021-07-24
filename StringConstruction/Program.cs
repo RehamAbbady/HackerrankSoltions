ec2aa04b257f49f236db12c843c9e7ab7a5126e3
Riham Abbady Ali <r_abbady@icloud.com>
2021-07-24 16:10:36 +0200

    .

--- StringConstruction/Program.cs
+++ StringConstruction/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringConstruction
 {
     class Program
     {
+        public static int stringConstruction(string s)
+        {
+            HashSet<char> uniqueChars = new HashSet<char>();
+            foreach (char c in s) {
+                uniqueChars.Add(c);
+            }
+            return uniqueChars.Count;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(stringConstruction("aabb"));
         }
     }
 }
